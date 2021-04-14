8a6d11ed1adb91dfe6f9060d50fb29d2b22834b4
frawalther <waltherf@hu-berlin.de>
2021-04-14 12:22:16 +0200

    edited

--- SCRIPT.R
+++ SCRIPT.R
@@ -1,12 +1,8 @@
-#GITHUB
-# use_git_config(user.name = "frawalther", user.email = "waltherf@hu-berlin.de")
-# https://github.com/frawalther/sanddam.git
 
-#
-remove.packages(c("sp", "sf", "rgdal", "rgrass7", "watershed"))
-install.packages(c("rgdal", "sp", "sf"))
-remotes::install_github("rsbivand/rgrass7", dependencies = TRUE)
-remotes::install_github("flee-group/watershed", ref="main", dependencies=TRUE)
+# remove.packages(c("sp", "sf", "rgdal", "rgrass7", "watershed"))
+# install.packages(c("rgdal", "sp", "sf"))
+# remotes::install_github("rsbivand/rgrass7", dependencies = TRUE)
+# remotes::install_github("flee-group/watershed", ref="main", dependencies=TRUE)
 
 #Load in libraries
 library(rgrass7)
@@ -15,26 +11,65 @@ library(dplyr)
 library(rgdal)
 library(rgeos)
 library(sf)
-#install.packages()
 library(watershed)
+library(Matrix)
 
-options(gisBase = "C:/PROGRA~1//QGIS3~1.16/apps/grass/grass78")
-#grass76 cannot be found
-#options(gisBase = "C:/PROGRA~1//QGIS3~1.16/apps/grass/grass76")
+#options(gisBase = "C:/PROGRA~1//QGIS3~1.18/apps/grass/grass78")
+options(gisBase = "C:/Program Files/GRASS GIS 7.8")
 
+#########################################
+############ TEST DATA ##################
+#########################################
+data(kamp_dem)
+kamp = delineate(kamp_dem)
+kamp_Tp = pixel_topology(kamp)
+## Warning in .check_topology(res, warn = TRUE): Invalid topology; 1 nodes are
+## downstream of more than two nodes.
+kv = vectorise_stream(kamp[["stream"]], Tp=kamp_Tp)
+## WARNING: Memory leak: 4 points are still in use
+plot(kamp_dem, col=terrain.colors(20), axes = FALSE)
+plot(st_geometry(kv), col='blue', add = TRUE)
+
+kamp_Tr = reach_topology(kamp, kamp_Tp)
+
+
+
+#############################
 #Digital Elevation Model 
 dem <- raster("~/01Master/MasterThesis/Pius/DEM/DEM_ext.tif", format="GTiff")
 
 dem_proj <- projectRaster(dem, crs="+proj=utm +zone=37 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0", method='ngb') 
-
+#writeRaster(dem_proj, "~/01Master/MasterThesis/Pius/DEM/dem_proj.tif", format="GTiff", overwrite=T)
 # hand digitized streams
 streams <- readOGR(dsn="~/01Master/MasterThesis/Pius/DEM", layer="stream_dig")
 
-execGRASS("r.carve", raster="dem_proj", vector="streams", output="dem_carved", width=30, depth=30)
+#Hands on GRASS:
+run <- FALSE
+if (nchar(Sys.getenv("GISRC")) > 0 &&
+    read.dcf(Sys.getenv("GISRC"))[1,"LOCATION_NAME"] == "nc_basic_spm_grass7") run <- TRUE
+    oechoCmd <- get.echoCmdOption()
+    set.echoCmdOption(TRUE)
+if (run) {
+  execGRASS("r.carve", raster="dem_proj", vector="streams", output="dem_carved", width=30, depth=30)
+}
+#ERROR: Raster map <dem_proj> not found
 
-#ERROR!: Grass commands do not run
-#Error in if (get("SYS", envir = .GRASS_CACHE) == "WinNat" && nchar(WN_bat) ==  : 
-#missing value where TRUE/FALSE needed
+
+
+
+
+dem_carved <- raster("~/01Master/MasterThesis/Pius/DEM/dem_carved.tif")
+
+#delineate stream network   
+
+kitui = delineate(dem_carved,threshold= 1e+05)
+#plot(dem_carved, col=terrain.colors(20), axes= FALSE)
+#plot(kitui@stream, col="blue", add=T, legend=F)
+
+
+
+
+
 
 
 
