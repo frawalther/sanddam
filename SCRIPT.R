37daca0c5dd6c88cb9752ab2f629634891f3c7d2
frawalther <waltherf@hu-berlin.de>
2021-04-22 12:42:07 +0200

    SD area delineation [fin]

--- SCRIPT.R
+++ SCRIPT.R
@@ -3,6 +3,7 @@
 # remotes::install_github("rsbivand/rgrass7", dependencies = TRUE)
 # remotes::install_github("flee-group/watershed", ref="main", dependencies=TRUE)
 # remotes::install_github("mtalluto/WatershedTools")
+
 #Load in libraries
 library(rgrass7)
 library(raster)
@@ -17,21 +18,21 @@ library(WatershedTools)
 #options(gisBase = "C:/PROGRA~1//QGIS3~1.18/apps/grass/grass78")
 options(gisBase = "C:/Program Files/GRASS GIS 7.8")
 
-      #########################################
-      ############ TEST DATA ##################
-      #########################################
-      data(kamp_dem)
-      kamp = delineate(kamp_dem)
-      kamp_Tp = pixel_topology(kamp)
-      ## Warning in .check_topology(res, warn = TRUE): Invalid topology; 1 nodes are
-      ## downstream of more than two nodes.
-      kv = vectorise_stream(kamp[["stream"]], Tp=kamp_Tp)
-      ## WARNING: Memory leak: 4 points are still in use
-      plot(kamp_dem, col=terrain.colors(20), axes = FALSE)
-      plot(st_geometry(kv), col='blue', add = TRUE)
-      
-      kamp_Tr = reach_topology(kamp, kamp_Tp)
-      #########################################
+# #########################################
+# ############ TEST DATA ##################
+# #########################################
+# data(kamp_dem)
+# kamp = delineate(kamp_dem)
+# kamp_Tp = pixel_topology(kamp)
+# ## Warning in .check_topology(res, warn = TRUE): Invalid topology; 1 nodes are
+# ## downstream of more than two nodes.
+# kv = vectorise_stream(kamp[["stream"]], Tp=kamp_Tp)
+# ## WARNING: Memory leak: 4 points are still in use
+# plot(kamp_dem, col=terrain.colors(20), axes = FALSE)
+# plot(st_geometry(kv), col='blue', add = TRUE)
+# 
+# kamp_Tr = reach_topology(kamp, kamp_Tp)
+# #########################################
 
 #Digital Elevation Model 
 dem <- raster("~/01Master/MasterThesis/Pius/DEM/DEM_ext.tif", format="GTiff")
@@ -52,28 +53,28 @@ dem_carved = raster(readRAST("dem_carved"))
 
 #writeRaster(dem_carved, "~/01Master/MasterThesis/Pius/DEM/d_carved.tif", format="GTiff")
 
-    #DELINEATE STREAM NETWORK 
-    #kitui = delineate(dem_carved, threshold=5e+04)# outlet=NA (default), set outlet=c(4704588, 2847762)
-            # Warning messages:
-            #   1: In delineate(dem_carved, threshold = 1e+06) :
-            #   Small threshold; excessive computation time and memory usage are possible if threshold not increased
-            # 2: In showSRID(SRS_string, format = "PROJ", multiline = "NO", prefer_proj = prefer_proj) :
-            #   Discarded datum unknown in Proj4 definition
-            # 3: In showSRID(SRS_string, format = "PROJ", multiline = "NO", prefer_proj = prefer_proj) :
-            #   Discarded datum unknown in Proj4 definition
-            # 4: In showSRID(SRS_string, format = "PROJ", multiline = "NO", prefer_proj = prefer_proj) :
-            #   Discarded datum unknown in Proj4 definition
-            # 5: In showSRID(SRS_string, format = "PROJ", multiline = "NO", prefer_proj = prefer_proj) :
-            #   Discarded datum unknown in Proj4 definition
-            
-    
-    #kitui_Tp = pixel_topology(kitui)
-    #kivec = vectorise_stream(kitui[["stream"]], Tp=kitui_Tp) #streams arent captured nicely with delineate(); for more exact delienation: extract_stream() // r.stream.extract()
-    #plot(dem_carved, col=terrain.colors(20), axes= FALSE)
-    #plot(st_geometry(kivec), col='blue', add = TRUE)
+#DELINEATE STREAM NETWORK 
+#kitui = delineate(dem_carved, threshold=5e+04)# outlet=NA (default), set outlet=c(4704588, 2847762)
+# Warning messages:
+#   1: In delineate(dem_carved, threshold = 1e+06) :
+#   Small threshold; excessive computation time and memory usage are possible if threshold not increased
+# 2: In showSRID(SRS_string, format = "PROJ", multiline = "NO", prefer_proj = prefer_proj) :
+#   Discarded datum unknown in Proj4 definition
+# 3: In showSRID(SRS_string, format = "PROJ", multiline = "NO", prefer_proj = prefer_proj) :
+#   Discarded datum unknown in Proj4 definition
+# 4: In showSRID(SRS_string, format = "PROJ", multiline = "NO", prefer_proj = prefer_proj) :
+#   Discarded datum unknown in Proj4 definition
+# 5: In showSRID(SRS_string, format = "PROJ", multiline = "NO", prefer_proj = prefer_proj) :
+#   Discarded datum unknown in Proj4 definition
 
 
-# STREAMS: r.stream.extract // [extract_stream() NOT THERE YET]
+#kitui_Tp = pixel_topology(kitui)
+#kivec = vectorise_stream(kitui[["stream"]], Tp=kitui_Tp) #streams arent captured nicely with delineate(); for more exact delienation: extract_stream() // r.stream.extract()
+#plot(dem_carved, col=terrain.colors(20), axes= FALSE)
+#plot(st_geometry(kivec), col='blue', add = TRUE)
+
+
+# STREAMS: r.stream.extract // [extract_stream() fOES NOT EXIST YET]
 execGRASS("r.watershed", parameters = list(elevation="dem_carved", accumulation="accu", drainage="drain"), 
           flags="overwrite")
 
@@ -94,22 +95,24 @@ sd_KWest <- SpatialPointsDataFrame(KWest[,3:4], KWest)
 crs(sd_KWest) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
 KWest_proj <- spTransform(sd_KWest, crs("+proj=utm +zone=37 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 
-        # writeVECT(KWest_proj, "KWest_proj", driver="ESRI Shapefile", v.in.ogr_flags="overwrite") 
-        # execGRASS("r.stream.snap", parameters=list(input="KWest_proj", output="KWest_snap", stream_rast="r_stream", accumulation="accu", threshold=200, radius=1),
-        #           flags="overwrite")
-        # 
-        # KWest_snap = readVECT("KWest_snap", type="point")
-        
-        # KWest_sd <- st_as_sf(KWest, coords= c("Point.X", "Point.Y"), crs=4326)
-        # KWest_sd <- st_transform(KWest_sd, 32737)
+# writeVECT(KWest_proj, "KWest_proj", driver="ESRI Shapefile", v.in.ogr_flags="overwrite") 
+# execGRASS("r.stream.snap", parameters=list(input="KWest_proj", output="KWest_snap", stream_rast="r_stream", accumulation="accu", threshold=200, radius=1),
+#           flags="overwrite")
+# 
+# KWest_snap = readVECT("KWest_snap", type="point")
+
+# KWest_sd <- st_as_sf(KWest, coords= c("Point.X", "Point.Y"), crs=4326)
+# KWest_sd <- st_transform(KWest_sd, 32737)
 
 KWest_snapped <- snapToStream(KWest_proj, r_stream, buff=100)
 
-#Create catchment area at each sand dam point
+################################################
+######## Catchment Delineation #################
+# create catchment area at each sand dam point #
 
-catchment(x=KWest_snapped, drainage=drain, output="sf") #area=T
-    #Error in system2("grass74", args = c("--config path"), stdout = TRUE) : 
-    #  '"grass74"' not found
+#catchment(x=KWest_snapped, drainage=drain, output="sf") #area=T
+#Error in system2("grass74", args = c("--config path"), stdout = TRUE) : 
+#  '"grass74"' not found
 
 result <- list()
 x<- sp::coordinates(KWest_snapped)
@@ -118,16 +121,55 @@ for(i in 1:nrow(x)) {
                      output = "catchment_areas", coordinates = x[i,])
   catchment_areas = raster(readRAST("catchment_areas"))
   rgrass7::execGRASS("r.to.vect", flags = c('overwrite', 'quiet'), input = "catchment_areas", 
-                   output = "ca_vect", type = 'area', column='value')
+                     output = "ca_vect", type = 'area', column='value')
   vect = sf::st_as_sf(rgrass7::readVECT("ca_vect", ignore.stderr = TRUE))
   result[[i]] = vect
 }
-#WARNINGs: Vector map <ca_vect> already exists and will be overwritten !!!
+#WARNINGs: Vector map <ca_vect> already exists and will be overwritten
 
 ca_all <- do.call(rbind, result) 
 ca_all <- cbind(ID = 1:nrow(ca_all), ca_all) 
 
 plot(st_geometry(ca_all))
+class(ca_all)
 
 
-     
+ca_all <- st_as_sf(ca_all)
+ca_all <- ca_all %>%
+  select(-cat, -value, -label)
+
+#save ca_all (so I do not need to re-run it)
+
+# st_write(dsn = "~/01Master/MasterThesis/Pius/geodata/ca_all.shp", layer = 'ca_all')
+#   # Error in UseMethod("st_write") : 
+#   #   no applicable method for 'st_write' applied to an object of class "character"
+
+ca_all <- as(ca_all, "Spatial")
+writeOGR(ca_all, dsn = "~/01Master/MasterThesis/Pius/geodata" , layer="ca_all" , driver = "ESRI Shapefile", overwrite_layer=T)
+
+cas <- readOGR(dsn="~/01Master/MasterThesis/Pius/geodata", layer="ca_all")
+cas <- st_as_sf(cas)
+###############
+### BUFFER ####
+###############
+
+#sand dam buffer
+KWest_snapped <- st_as_sf(KWest_snapped)
+sdbuf <- st_buffer(KWest_snapped, 500)
+
+#intersecting sand dam buffer with catchment areas
+sd_ca <- st_intersection(sdbuf, cas)
+sd_ca_int <- sd_ca[which(sd_ca$ID == sd_ca$ID.1),]
+
+plot(st_geometry(sd_ca))
+plot(st_geometry(sd_ca_int))
+
+#river buffer
+stream <- st_as_sf(v_stream) #or r_stream?
+rivbuf <- st_buffer(stream, 100) #check distance decision again! 
+rivbuf <- st_union(rivbuf) 
+
+#intersect
+sd_area <- st_intersection(sd_ca_int, rivbuf)
+plot(st_geometry(sd_area))
+
