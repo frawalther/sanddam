5152306330beb80bc35dd7b761addac06f1903ea
frawalther <waltherf@hu-berlin.de>
2021-04-29 10:24:00 +0200

    d

--- SCRIPT.R
+++ SCRIPT.R
@@ -106,9 +106,9 @@ KWest_proj <- spTransform(sd_KWest, crs("+proj=utm +zone=37 +south +datum=WGS84 
 
 KWest_snapped <- snapToStream(KWest_proj, r_stream, buff=100)
 
-################################################
-######## Catchment Delineation #################
-# create catchment area at each sand dam point #
+#################################
+##### Catchment Delineation #####
+#################################
 
 #catchment(x=KWest_snapped, drainage=drain, output="sf") #area=T
     #Error in system2("grass74", args = c("--config path"), stdout = TRUE) : 
@@ -149,6 +149,7 @@ ca_all <- ca_all %>%
 
 cas <- readOGR(dsn="~/01Master/MasterThesis/Pius/geodata", layer="ca_all")
 cas <- st_as_sf(cas)
+
 ###############
 ### BUFFER ####
 ###############
@@ -172,4 +173,190 @@ rivbuf <- st_union(rivbuf)
 #intersect
 sd_area <- st_intersection(sd_ca_int, rivbuf)
 plot(st_geometry(sd_area))
+
+st_write(sd_area, dsn = "~/01Master/MasterThesis/Pius/geodata", layer = 'sd_area', driver="ESRI Shapefile") #check again
+
+sd_area <- st_read(dsn = "~/01Master/MasterThesis/Pius/geodata", layer = 'sd_area')
+sd_area <- as(sd_area, "Spatial")
+
+
+#######################################################################################################
+##### Land Cover ###############
+################################
+
+LC <- raster("~/01Master/MasterThesis/Pius/geodata/LC_extentrough.tif")
+crs(LC) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+
+proj <- "+proj=utm +zone=37 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
+
+LC_proj <- projectRaster(LC, crs=proj, method='ngb') #painly slow 
+writeRaster(LC_proj, "~/01Master/MasterThesis/Pius/geodata/LC_proj.tif", format="GTiff", overwrite=T)
+
+## crop and mask
+LC_r <- crop(LC_proj, extent(sd_area))
+LC_r <- mask(LC_r, sd_area)
+
+#only cropland (4)
+unique(LC_r)
+#[1] 2 4 5 8
+
+    cropland<-LC_r
+    values(cropland)[values(cropland) != 4] = NA
+    LC_crop <- rasterToPolygons(cropland, dissolve = T, na.rm=T)
+    LC_crop <- st_as_sf(LC_crop)
+    crop_int <- st_intersection(LC_crop, sd_area)
+    crop_int <- crop_int[which(crop_int$ID == crop_int$ID_1),]
+    crop_int$class <- "cropland"
+    #writeRaster(cropland, "~/01Master/MasterThesis/Pius/geodata/Cropland.tif", format="GTiff", overwrite=T)
+    
+    shrubs<-LC_r
+    values(shrubs)[values(shrubs) != 2] = NA
+    LC_shrub <- rasterToPolygons(shrubs, dissolve = T, na.rm=T)
+    LC_shrub <- st_as_sf(LC_shrub)
+    shrub_int <- st_intersection(LC_shrub, sd_area)
+    shrub_int <- shrub_int[which(shrub_int$ID == shrub_int$ID_1),]
+    shrub_int$class <- "shrubs"
+    #writeRaster(shrubs, "~/01Master/MasterThesis/Pius/geodata/shrubs.tif", format="GTiff", overwrite=T)
+    
+    veg_aqua<-LC_r
+    values(veg_aqua)[values(veg_aqua) != 5] = NA
+    LC_vega <- rasterToPolygons(veg_aqua, dissolve = T, na.rm=T)
+    LC_vega <- st_as_sf(LC_vega)
+    vega_int <- st_intersection(LC_vega, sd_area)
+    vega_int <- vega_int[which(vega_int$ID == vega_int$ID_1),]
+    vega_int$class <- "veg_aqua"
+    #writeRaster(veg_aqua, "~/01Master/MasterThesis/Pius/geodata/veg_aqua.tif", format="GTiff", overwrite=T)
+    #very small fraction! Does it fit to another lc?
+    
+    #exclude built up areas [8]
+    #write in a loop?
+    
+#combine them into one dataframe
+lc_int <- rbind(crop_int, shrub_int, vega_int)
+
+#calculate area for each sf feature (basis to e.g. calculate overall mean EVI/SD)
+lc_int$area_m2 <- st_area(lc_int)
+
+
+###############################################################################################################
+#### VEGETATION EVI Data #######
+################################
+
+EVI_list <- list.files("~/01Master/MasterThesis/Pius/NDVI_EVI", pattern='.tif$', recursive=T, full.names = T) #FW_EVI_2014-2020 #839
+EVI_list
+
+#Create reference raster to set extent
+e <- readOGR(dsn="~/01Master/MasterThesis/Pius/climatedata", layer="extent_rough")
+e_proj <- spTransform(e, crs("+proj=utm +zone=37 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+
+ref<-raster()
+extent(ref) <- extent(e_proj)
+res(ref) <- c(30,30)
+crs(ref) <- "+proj=utm +zone=37 +south +datum=WGS84 +units=m +no_defs"
+
+          # #LAPPLY (Issue: running into memory shortage)
+                # list_r <- lapply(EVI_list, raster)
+                # r_proj <- lapply(list_r, projectRaster, to=ref, res=c(30,30), 
+                #                  crs="+proj=utm +zone=37 +south +datum=WGS84 +units=m +no_defs", 
+                #                  method="bilinear")
+                # #Error: cannot allocate vector of size N Mb
+                # #There were 50 or more warnings (use warnings() to see the first 50)
+                # 
+                # ex_r <- lapply(list_r, extend, y=extent(ref), value=NA)
+                # ##Error: cannot allocate vector of size N Mb
+                # #crop_r <-lapply(list_r, crop, y=ref)
+
+#instead: Writing LOOP (run functions for each object (i) in list and respectively write Raster) 
+
+#garbage collection 
+gc()
+        # outpath <- "~/01Master/MasterThesis/Pius/NDVI_EVI/t"
+        # dir.create(outpath)
+        # outfiles <- paste0(outpath, EVI_list)
+#outfiles <-list()
+for(i in 1:length(EVI_list)) {
+  r <-raster(EVI_list[[i]])
+  r_proj <- projectRaster( from=r, to=ref, res=c(30,30), 
+                           crs="+proj=utm +zone=37 +south +datum=WGS84 +units=m +no_defs", 
+                           method="bilinear")
+  rc <- writeRaster(r_proj, paste("outfiles",i,".tif",sep=""), overwrite=T)
+  removeTmpFiles(0.1)
+}
+
+EVI_files <- list.files("C:/Users/franz/Documents/01Master/MasterThesis/Pius/R/sand dam/", pattern='outfiles', recursive=T, full.names = T)
+
+#stack files 
+EVI_rasters <- raster::stack(EVI_files)
+plot(EVI_rasters$outfiles1)
+
+#brick
+EVI_brick <- brick(EVI_rasters)
+
+stackSave(EVI_rasters, "C:/Users/franz/Documents/01Master/MasterThesis/Pius/R/sand dam/EVIstack.stk")
+writeRaster(EVI_rasters, "C:/Users/franz/Documents/01Master/MasterThesis/Pius/R/sand dam/EVIstack.tif", format="GTiff")
+
+
+#comments:
+# executing loop function took >2-3hours
+#lost important filenames (nicluding date etc.)
+# zu umständlich? 
+
+s <- stackOpen("C:/Users/franz/Documents/01Master/MasterThesis/Pius/R/sand dam/EVIstack.stk")
+
+plot(s$outfiles700)
+head(s@z)
+
+
+#extract date from filenames 
+library(lubridate)
+
+split_list = strsplit(EVI_list, split="_", fixed=TRUE)
+split_col = unlist(lapply(split_list, "[[", 9))
+
+date <- lubridate::ymd(basename(split_col))
+
+###################################################################### 
+
+mini_list <- list()
+for (i in minikin_file[1:4]) {
+  print(read_csv2(i))
+  print(str_split(i, "/", simplify = T)[,12])
+  mini_list[[i]] <- read_csv2(i) %>% 
+    mutate(sensor = str_split(i, "/", simplify = T)[,12])
+}
+
+
+
+
+
+#Stack or Brick Rasters 
+b <- brick(EVI_rasters)
+EVI_rasters <- raster::stack(EVI_list, quick=F) 
+EVI_rasters <- brick(xxx)
+
+#### Zonal statistics ####
+?zonal
+
+
+##################### Error: cannot allocate vector of size N Mb
+### MEMORY ISSUE #### How to improve R performance? 
+#####################
+
+#garbage collection
+  # gc() 
+#check memory limit
+  # memory.limit()  
+  # [1] 16182
+  #memory.size()
+#rm()
+#check RAM 
+  # library(benchmarkme)
+  # benchmarkme::get_ram()
+#enable working on multiple cores in parallel [increases memory usage]
+  # library(doParallel)
+  # > detectCores()
+  # [1] 8
+  #mclapply() [only works for Mac - instead parLapply]
+  #numCores <- detectCores()-1
+
 
