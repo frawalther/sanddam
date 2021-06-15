3f27a3d180170facc7f75da6696fa1d33c884a9d
frawalther <waltherf@hu-berlin.de>
2021-06-15 10:36:01 +0200

    R execution script for stan models

---
+++ modelling.R
@@ -0,0 +1,96 @@
+
+#packages of interest 
+library(rstan)
+library(bayesplot)
+library(loo)
+library(brms)
+library(rtsanarm)
+library(atsar)
+
+#packages
+library(dplyr)
+library(ggplot2)
+
+#load dataset
+load("df_com.RData")
+head(df_com)
+
+#autoregressive model
+#Stan AR(1)
+#m1_AR
+n <- nrow(df_com)
+#or: 
+# unique(df_com$X)
+# n <- 286 #dont think so 
+
+fit_AR1 <- stan(file="m1_AR.stan", 
+                data = list(N = n,
+                            y = df_com$EVI_mean),
+                chains=4, 
+                cores=4,
+                iter=2000)
+summary(fit_AR1)
+posterior1 <- as.matrix(fit_AR1)
+head(posterior1)
+
+
+#Gaussian process:
+# marginal likelihood GP 
+n <- nrow(df_com)
+fit_GP1 <- stan(file="GP_1.stan",
+                data = list(N = n,
+                            evi = df_com$EVI_mean,
+                            P = df_com$Precip_mean, 
+                            t = df_com$timeorder),
+                chains=4, 
+                cores=4,
+                iter=2000)
+
+#need to include time stemps - time order where? 
+
+#Latent variable GP 
+n<- nrow(df_com)
+
+fit_GP2 <- stan(file="GP_2.stan",
+                data = list(N=n,
+                            evi = df_com$EVI_mean,
+                            P = df_com$Precip_mean,
+                            t = df_com$timeorder),
+                chains=4, 
+                cores=4,
+                iter=2000)
+
+
+# 
+# #m2_AR
+# n <- nrow(df_com)
+# unique(df_com$LC_proj)
+# n_j <- 3
+# 
+# fit_AR1_m2 <- stan(file="m2_AR.stan", 
+#                 data = list(N = n,
+#                             x = df_cc$Precip_mean,
+#                             y = df_cc$EVI_mean,
+#                             J = n_j,
+#                             group = df_cc$LC_proj),
+#                 chains=4, 
+#                 cores=4,
+#                 iter=2000)
+# 
+# summary(fit_AR1_m2)
+# posterior2 <- as.matrix(fit_AR1_m2)
+
+
+# #brms
+# #AR(1)
+# lm_ar<- brms::brm(data=df_cc,
+#                   family=gaussian(),
+#                   formula= log(EVI_mean) ~ arma(gr=LC_proj, p=1, q=1) + Precip_mean , #time= #cov=T 
+#                   prior= prior(normal(0,1), class=b),
+#                   sample_prior=T,
+#                   cores=7,
+#                   chains=2,
+#                   iter=2000)
+# 
+# ?brm
+# ?arma
