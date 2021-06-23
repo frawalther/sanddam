bfac182500832f1855a319275203dca35bf1302e
frawalther <waltherf@hu-berlin.de>
2021-06-23 16:39:36 +0200

    updated miniGP2

--- GP_2.stan
+++ GP_2.stan
@@ -1,17 +1,40 @@
 // latent variable GP 
 data {
   int<lower=1> N; //nrows(df)
-  vector<lower = -1, upper = 1>[N] evi;//EVI, y [N] or [t] ? 
+  vector<lower = -1, upper = 1>[N] evi;//EVI(mean)
   vector [N] P; //Precipitation(mean)
   real time [N];//time order
+  
   // int<lower=1> LC [N];
   // int n_lc;
   //matrix [N,k] x;
+  
+  int<lower=1> ngp; //Number of GPs to fit 
+  int<lower=1, upper = ngp> gp_id [N]; //sampling unit ID, allowing for one GP per 
+  int gp_sampsize [ngp]; //for each GP, how many data points are represented 
+  int<lower=1, upper=N> max_gp_sampsize; // how many samples in the largest covariance kernel
 
 }
 
 transformed data {
   real delta = 1e-9;
+  
+  real gp_time [ngp, max_gp_sampsize];
+  // we need a way to get back to the original index in order to fill in gamma later
+  int gp_index[ngp, max_gp_sampsize];
+  for(i in 1:ngp) {
+    gp_time[i] = rep_array(0, max_gp_sampsize);
+    gp_index[i] = rep_array(0, max_gp_sampsize);
+  }
+  
+  {
+    int last [N] = rep_array(0,N);
+    for(i in 1:N) {
+      gp_time[gp_id[i]][last[i] +1] = time[i];
+      gp_index[gp_id[i]][last[i] +1] = i;
+      //last[i] += 1; // stopped here 
+    }
+  }
 }
 
 parameters {
@@ -30,17 +53,32 @@ parameters {
 
 transformed parameters {
   vector [N] mu;
-  {    
-    //compute variance-covariance matrix
-    matrix[N,N] K = cov_exp_quad(time, alpha, rho); 
-    matrix[N,N] L_K; //cholesky decomposition of VCV matrix (lower triangle)
+  {
     vector [N] gamma; //additive effect of GP
-    
-    //diagonal elements
-    for (i in 1:N)
-      K[i,i] = K[i,i] + delta;
+    // fit the GPs
+    for(i in 1:ngp) {
+      int ss = gp_sampsize[i];
+      vector [ss] et_gp; // local copy of eta for just this GP
+      vector [ss] gam_gp; //local copy of gamma for just this GP
+      
+          //compute variance-covariance matrix
+      matrix[ss,ss] K = cov_exp_quad(gp_time[i, 1:ss], alpha, rho); 
+      matrix[ss,ss] L_K; //cholesky decomposition of VCV matrix (lower triangle)
+
+      //diagonals
+      // find the right values for eta 
+      for (j in 1:ss)
+        K[j,j] = K[j,j] + delta;
+        et_gp[j] = eta[gp_index[i,j]];
+    }
+   
     L_K = cholesky_decompose(K);
-    gamma = L_K * eta;
+    gam_gp = L_K * et_gp;
+    for (j in 1:ss) {
+    gamma[gp_index[i,j]] = gam_gp[j];
+    }
+    
+    // add GP effect to the linear model
     mu = gamma + a + b * P;
   }
 }
--- modelling.R
+++ modelling.R
@@ -1,15 +1,15 @@
 #packages
 library(rstan)
 library(dplyr)
+library(ggplot2)
+library(rstanarm)
+library(bayesplot)
 
 #packages of interest 
-library(bayesplot)
+
 library(loo)
 library(brms)
-library(rtsanarm)
 library(atsar)
-
-library(ggplot2)
 
 
 #load dataset
@@ -21,25 +21,113 @@ unique(df_com$X) #286
 #exclude LC "veg_aqua"
 df_par <- df_com %>%
   filter (class != "veg_aqua")
-nrow(df_par)
-unique(df_par$X) #269
+
+#cropland
+df_crop <- df_com %>%
+  filter (class == "cropland")
+
+nrow(df_crop) #n=34314
+unique(df_crop$X) #n=135
+
+#shrubs
+df_shrubs <- df_com %>%
+  filter (class == "shrubs")
+
+nrow(df_shrubs) #n=31253
+unique(df_shrubs$X) #n=134
+
 
 #Gaussian process:
-  # marginal likelihood GP 
-  n <- nrow(df_com)
+
+#Matts
+
+#Latent variable GP
+#add something to ID individual time series 
+df_par$gp_id = paste(df_par$X, df_par$class, sep="_")
+
+#try a subset of data first
+standat = with(df_par[df_par$X <= 3,], list(
+  evi = EVI_mean,
+  P = Precip_mean,
+  time = timeorder,
+  gp_id = as.integer(factor(gp_id))
+))
+
+standat$gp_sampsize = table(standat$gp_id)
+standat$max_gp_sampsize = max(standat$gp_sampsize)
+standat$ngp = max(standat$gp_id)
+standat$N = length(standat$evi)
+
+GP2 = stan_model("GP_2.stan")
+fit_GP2 = sampling (GP2, data=standat, 
+                    chains=1,
+                    cores=4, 
+                    iter=2000)
+
+
+# for 1 SD 
+  df_1 <- df_com %>%
+    filter(ID == 1) #or X ==1 
+  n <- nrow(df_1)
   #t = df_com$timeorder
   fit_GP1 <- stan(file="GP_1.stan",
                   data = list(N = n,
-                              evi = df_com$EVI_mean,
-                              P = df_com$Precip_mean),
-                              #t = df_com$timeorder),
+                              evi = df_1$EVI_mean,
+                              P = df_1$Precip_mean,
+                              time = df_1$timeorder, 
+                              LC = df_1$LC_proj),
                   chains=1, 
                   cores=4,
                   iter=2000)
 
-#need to include time steps - time order where? 
+  #need to repeat that for all Sds/X
 
+  summary(fit_GP1)
+  glimpse(fit_GP1)
+  #Rhat
+    rhat_GP1 <- rhat(fit_GP1)
+    mcmc_rhat_hist(rhat_GP1)
+  #Neff
+    ratios_GP1 <- neff_ratio(fit_GP1)
+    mcmc_neff(ratios_GP1)
+  #traceplot
+    traceplot(fit_GP1, pars= c("rho", "alpha", "sigma", "a", "b")) #Which parameter are of interest?
+    #or
+    mcmc_trace(fit_GP1, pars= c("rho", "alpha", "sigma", "a", "b")) #Which parameter are of interest?
+
+  #posterior predictive checks: WHICH FUNCTION?
+    posterior_samples()
+    posterior_predict()
+    fitted()
+    samples()
+    
+  posterior <- as.matrix(fit_GP1)
+  #or?
+  post <- posterior_predict(fit_GP1, 100)
+  mcmc_areas(posterior) # no good visualization 
+  ?posterior_predict(posterior )
+  ?sampling()
+  pp_check(fit_GP1, nsamples = 100, type = "hist")
+  bayesplot::pp_check(fit_GP1, plotfun="dens_overlay", nreps=100)
+  #bayesplot() package
+  #Extract posterior draws from stanfit object
+  GP1_draws <- extract(fit_GP1)
+  # Traceplot 
+  mcmc_trace(GP1_draws) #pars = c("mean", "sd", "sigma")
+  ### Random draws from the full model compared to observed data
+  color_scheme_set("red")
+  ppc_dens_overlay(y = fit_GP1$y,
+                   yrep = posterior_predict(fit_GP1, draws = 50))
+  post_pred <- posterior_predict(fit, draws = 30) # Why is no transformation needed here?
+  
+  library(bayesplot)
+  
+  ppc_dens_overlay(y = fit$y, yrep = post_pred)
+  
+   
 #Latent variable GP 
+  #subsample  
+df_par = df_crop[sample(nrow(df_crop),200),]
   n<- nrow(df_par)
    
   GP2 = stan_model("GP_2.stan")
@@ -51,58 +139,9 @@ unique(df_par$X) #269
                      cores=4,
                      iter=2000)
 
+#wrong model: need to explicitly calculate VCV matrix per site 
 
-
-### TRASH ####
-# #autoregressive model
-# #Stan AR(1)
-# #m1_AR
-# n <- nrow(df_com)
-# #or: 
-# # unique(df_com$X)
-# # n <- 286 #dont think so 
-# 
-# fit_AR1 <- stan(file="m1_AR.stan", 
-#                 data = list(N = n,
-#                             y = df_com$EVI_mean),
-#                 chains=4, 
-#                 cores=4,
-#                 iter=2000)
-# summary(fit_AR1)
-# posterior1 <- as.matrix(fit_AR1)
-# head(posterior1)
-# 
-
-# 
-# #m2_AR
-# n <- nrow(df_com)
-# unique(df_com$LC_proj)
-# n_j <- 3
-# 
-# fit_AR1_m2 <- stan(file="m2_AR.stan", 
-#                 data = list(N = n,
-#                             x = df_cc$Precip_mean,
-#                             y = df_cc$EVI_mean,
-#                             J = n_j,
-#                             group = df_cc$LC_proj),
-#                 chains=4, 
-#                 cores=4,
-#                 iter=2000)
-# 
-# summary(fit_AR1_m2)
-# posterior2 <- as.matrix(fit_AR1_m2)
-
-
-# #brms
-# #AR(1)
-# lm_ar<- brms::brm(data=df_cc,
-#                   family=gaussian(),
-#                   formula= log(EVI_mean) ~ arma(gr=LC_proj, p=1, q=1) + Precip_mean , #time= #cov=T 
-#                   prior= prior(normal(0,1), class=b),
-#                   sample_prior=T,
-#                   cores=7,
-#                   chains=2,
-#                   iter=2000)
-# 
-# ?brm
-# ?arma
+  summary(fit_GP2)
+  posterior_GP2 <- as.matrix(fit_GP2)
+  head(posterior_GP2)
+  
