e02c202b6f4fe9b5550489d6f471043f9da592a0
frawalther <waltherf@hu-berlin.de>
2021-06-30 13:42:02 +0200

    GP1 (model for 1 Sand dam) - real delta -> vector jitter

--- GP_1.stan
+++ GP_1.stan
@@ -9,7 +9,8 @@ data {
 }
 
 transformed data {
-real delta = 1e-9;  
+//real delta = 1e-9;  
+vector[N] jitter = rep_vector(1e-9, N);
 }
 
 parameters {
@@ -30,23 +31,23 @@ transformed parameters {
   {    
     //compute variance-covariance matrix
     matrix[N,N] K = cov_exp_quad(time, alpha, rho); 
-    matrix[N,N] L_K; //cholesky decomposition of VCV matrix (lower triangle)
-    vector[N] gamma; //additive effect of GP
+    matrix[N,N] L_K = cholesky_decompose(add_diag(K, jitter)); //cholesky decomposition of VCV matrix (lower triangle)
+    vector[N] gamma = L_K * eta; //additive effect of GP
     
-    //diagonal elements
-    for (i in 1:N) { 
-      K[i,i] = K[i,i] + delta;
-    L_K = cholesky_decompose(K); 
-    gamma = L_K * eta;
+    // //diagonal elements
+    // for (i in 1:N) { 
+    //   K[i,i] = K[i,i] + delta;
+    // L_K = cholesky_decompose(K); 
+    // gamma = L_K * eta;
     mu = gamma + a + b1 * P; // + b2 * LC
-    }
+    //}
   }
 }
 
 model {
   rho ~ inv_gamma(5,5);
-  alpha~ std_normal();
-  eta~std_normal();
+  alpha ~ std_normal();
+  eta ~ std_normal();
   a ~ normal(0,10);
   b1 ~ normal(0,10);
   //b2 ~ normal(0,10);
--- modelling.R
+++ modelling.R
@@ -63,85 +63,91 @@ fit_GP2 = sampling (GP2, data=standat,
                     chains=1,
                     cores=4, 
                     iter=2000)
+summary(fit_GP2)
+
+head(df_1)
+### for 1 SD 
+  df_1 <- df_par %>%
+    filter(ID == 1) #%>%
+    filter(class == "cropland")
 
 
-# for 1 SD 
-  df_1 <- df_com %>%
-    filter(ID == 1) #or X ==1 
+      #or X ==1 
   n <- nrow(df_1)
   #t = df_com$timeorder
   fit_GP1 <- stan(file="GP_1.stan",
                   data = list(N = n,
                               evi = df_1$EVI_mean,
                               P = df_1$Precip_mean,
-                              time = df_1$timeorder, 
-                              LC = df_1$LC_proj),
+                              time = df_1$timeorder), 
+                              #LC = df_1$LC_proj),
                   chains=1, 
                   cores=4,
                   iter=2000)
 
   #need to repeat that for all Sds/X
 
-  summary(fit_GP1)
-  glimpse(fit_GP1)
-  #Rhat
-    rhat_GP1 <- rhat(fit_GP1)
-    mcmc_rhat_hist(rhat_GP1)
-  #Neff
-    ratios_GP1 <- neff_ratio(fit_GP1)
-    mcmc_neff(ratios_GP1)
-  #traceplot
-    traceplot(fit_GP1, pars= c("rho", "alpha", "sigma", "a", "b")) #Which parameter are of interest?
-    #or
-    mcmc_trace(fit_GP1, pars= c("rho", "alpha", "sigma", "a", "b")) #Which parameter are of interest?
+summary(fit_GP1)
 
-  #posterior predictive checks: WHICH FUNCTION?
-    posterior_samples()
-    posterior_predict()
-    fitted()
-    samples()
-    
-  posterior <- as.matrix(fit_GP1)
-  #or?
-  post <- posterior_predict(fit_GP1, 100)
-  mcmc_areas(posterior) # no good visualization 
-  ?posterior_predict(posterior )
-  ?sampling()
-  pp_check(fit_GP1, nsamples = 100, type = "hist")
-  bayesplot::pp_check(fit_GP1, plotfun="dens_overlay", nreps=100)
-  #bayesplot() package
-  #Extract posterior draws from stanfit object
-  GP1_draws <- extract(fit_GP1)
-  # Traceplot 
-  mcmc_trace(GP1_draws) #pars = c("mean", "sd", "sigma")
-  ### Random draws from the full model compared to observed data
-  color_scheme_set("red")
-  ppc_dens_overlay(y = fit_GP1$y,
-                   yrep = posterior_predict(fit_GP1, draws = 50))
-  post_pred <- posterior_predict(fit, draws = 30) # Why is no transformation needed here?
-  
-  library(bayesplot)
-  
-  ppc_dens_overlay(y = fit$y, yrep = post_pred)
-  
-   
-#Latent variable GP 
-  #subsample  
-df_par = df_crop[sample(nrow(df_crop),200),]
-  n<- nrow(df_par)
-   
-  GP2 = stan_model("GP_2.stan")
-  fit_GP2 = sampling(GP2, data = list(N=n,
-                                      evi = df_par$EVI_mean,
-                                      P = df_par$Precip_mean,
-                                      time= df_par$timeorder),
-                     chains=1,
-                     cores=4,
-                     iter=2000)
-
-#wrong model: need to explicitly calculate VCV matrix per site 
-
-  summary(fit_GP2)
-  posterior_GP2 <- as.matrix(fit_GP2)
-  head(posterior_GP2)
-  
+#   glimpse(fit_GP1)
+#   #Rhat
+#     rhat_GP1 <- rhat(fit_GP1)
+#     mcmc_rhat_hist(rhat_GP1)
+#   #Neff
+#     ratios_GP1 <- neff_ratio(fit_GP1)
+#     mcmc_neff(ratios_GP1)
+#   #traceplot
+#     traceplot(fit_GP1, pars= c("rho", "alpha", "sigma", "a", "b")) #Which parameter are of interest?
+#     #or
+#     mcmc_trace(fit_GP1, pars= c("rho", "alpha", "sigma", "a", "b")) #Which parameter are of interest?
+# 
+#   #posterior predictive checks: WHICH FUNCTION?
+#     posterior_samples()
+#     posterior_predict()
+#     fitted()
+#     samples()
+#     
+#   posterior <- as.matrix(fit_GP1)
+#   #or?
+#   post <- posterior_predict(fit_GP1, 100)
+#   mcmc_areas(posterior) # no good visualization 
+#   ?posterior_predict(posterior )
+#   ?sampling()
+#   pp_check(fit_GP1, nsamples = 100, type = "hist")
+#   bayesplot::pp_check(fit_GP1, plotfun="dens_overlay", nreps=100)
+#   #bayesplot() package
+#   #Extract posterior draws from stanfit object
+#   GP1_draws <- extract(fit_GP1)
+#   # Traceplot 
+#   mcmc_trace(GP1_draws) #pars = c("mean", "sd", "sigma")
+#   ### Random draws from the full model compared to observed data
+#   color_scheme_set("red")
+#   ppc_dens_overlay(y = fit_GP1$y,
+#                    yrep = posterior_predict(fit_GP1, draws = 50))
+#   post_pred <- posterior_predict(fit, draws = 30) # Why is no transformation needed here?
+#   
+#   library(bayesplot)
+#   
+#   ppc_dens_overlay(y = fit$y, yrep = post_pred)
+#   
+#    
+# #Latent variable GP 
+#   #subsample  
+# df_par = df_crop[sample(nrow(df_crop),200),]
+#   n<- nrow(df_par)
+#    
+#   GP2 = stan_model("GP_2.stan")
+#   fit_GP2 = sampling(GP2, data = list(N=n,
+#                                       evi = df_par$EVI_mean,
+#                                       P = df_par$Precip_mean,
+#                                       time= df_par$timeorder),
+#                      chains=1,
+#                      cores=4,
+#                      iter=2000)
+# 
+# #wrong model: need to explicitly calculate VCV matrix per site 
+# 
+#   summary(fit_GP2)
+#   posterior_GP2 <- as.matrix(fit_GP2)
+#   head(posterior_GP2)
+#   
